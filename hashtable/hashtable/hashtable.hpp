c4236eff4a0300cb03624c35f4a2ca0b78409545
文国邦 <handsomewgb@163.com>
2017-06-04 20:21:57 +0800

    用分类型的类模板参数解决容量开辟问题

--- hashtable/hashtable/hashtable.hpp
+++ hashtable/hashtable/hashtable.hpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-
+#include <math.h>
 enum State
 {
 	EMPTY,EXIST,DELETE
@@ -16,8 +16,9 @@ struct HashNode
 		:_s(EMPTY)
 	{}
 };
-
-template <class K, class V>
+//1.第一次开辟空间的大小用非类型的类模板参数解决
+//2.重新开辟空间的大小用获取质数函数解决
+template <class K, class V,size_t SIZE = 11>
 class Hashtable
 {
 	typedef pair<HashNode<K, V>*, bool> Pair;
@@ -25,6 +26,11 @@ public:
 	Hashtable()
 		:_size(0)
 	{}
+	Hashtable(size_t size)
+		:_size(0)
+	{
+		_ht.resize(size);
+	}
 	bool Insert(const K& key,const V& value)
 	{
 		Checkcapacity();
@@ -61,7 +67,7 @@ public:
 			{
 				if (elem._s == EXIST)
 					return Pair(&elem, true);
-				else if (elem._s == DELETE)
+				else //if (elem._s == DELETE)
 					return Pair(NULL, false);
 			}
 			++index;
@@ -93,7 +99,40 @@ private:
 	void Checkcapacity()
 	{
 		if (_size == _ht.size())
-			_ht.resize(P);
+			_ht.resize(SIZE);//用非类型的类模板参数解决
+		else if (_size * 10 / _ht.size() == 7)//(_size / _ht.size() == 0.7)
+		{//@荷载因子 = 有效元素个数/散列表长度
+			size_t Newsize = Prime_num(_ht.size() * 2);//重新开辟空间的大小用获取质数函数解决
+			Hashtable<K, V> ht(Newsize);
+			for (size_t i = 0; i < _ht.size(); ++i)
+			{
+				if (_ht[i]._s == EXIST)
+					ht.Insert(_ht[i]._hn.first, _ht[i]._hn.second);
+			}
+			//两个swap的运用
+			swap(_size,ht._size);
+			ht._ht.swap(_ht);
+		}
+	}
+
+	//求质数
+	size_t Prime_num(size_t size)
+	{
+		size_t Size = size;
+		while (Is_prime(Size) == false)
+			++Size;
+		return Size;
+	}
+	bool Is_prime(size_t size)
+	{
+		size_t i = 2;
+		while (i <= (sqrt(size)))
+		{
+			if (size%i == 0)
+				return false;
+			++i;
+		}
+		return true;
 	}
 private:
 	vector<HashNode<K,V>> _ht;
@@ -101,41 +140,23 @@ private:
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void Test()
 {
 	int a[] = {37,25,14,36,49,68,57,11};
-	Hashtable<int, int> ht();
+	Hashtable<int, int> ht;
 	for (size_t index = 0; index < sizeof(a) / sizeof(a[0]); ++index)
 	{
 		ht.Insert(a[index],index);
 	}
 	ht.Insert(24,8);
-	ht.Insert(42, 9);
-	ht.Insert(66, 10);
-	ht.Insert(78, 11);
-	pair<HashNode<int,int>*, bool> h = ht.Find(14);
-	pair<HashNode<int, int>*, bool> h1 = ht.Find(15);
+	//ht.Insert(42, 9);
+	//ht.Insert(66, 10);
+	//ht.Insert(78, 11);
+	//pair<HashNode<int,int>*, bool> h = ht.Find(14);
+	//pair<HashNode<int, int>*, bool> h1 = ht.Find(15);
 
-	if (ht.Remove(14))
-		cout << "True" << endl;
-	else
-		cout << "False" << endl;
-
+	//if (ht.Remove(14))
+	//	cout << "True" << endl;
+	//else
+	//	cout << "False" << endl;
 }
