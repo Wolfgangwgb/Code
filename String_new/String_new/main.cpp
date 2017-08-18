34d09ecb70d76392be12c4be761f47054455d745
文国邦 <handsomewgb@163.com>
2017-08-18 15:57:42 +0800

    写时拷贝

--- String_new/String_new/main.cpp
+++ String_new/String_new/main.cpp
@@ -2,7 +2,7 @@
 #include"string_ref_int.h"
 #include"string_ref_static.h"
 #include"string_ref_pointer.h"
-
+#include"string_copy_on_write.h"
 
 int main()
 {
@@ -10,5 +10,6 @@ int main()
 	Test_int_count();
 	Test_static_count();
 	Test_pointer_count();
+	Test_copy();
 	return 0;
 }
