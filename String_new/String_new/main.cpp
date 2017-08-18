ae8001ea0879ebfa1ec00f62a8f75f3a35c09175
文国邦 <handsomewgb@163.com>
2017-08-18 16:03:05 +0800

    深拷贝简洁版1

--- String_new/String_new/main.cpp
+++ String_new/String_new/main.cpp
@@ -4,6 +4,7 @@
 #include"string_ref_pointer.h"
 #include"string_copy_on_write.h"
 #include"string_deep_copy_one.h"
+#include"string_deep_copy_second.h"
 
 int main()
 {
@@ -13,5 +14,6 @@ int main()
 	Test_pointer_count();
 	Test_copy();
 	Test_d1();
+	Test_d2();
 	return 0;
 }
