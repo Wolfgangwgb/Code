83a14e69fcc7c9150d42a2d18d8e06344b4f6d20
文国邦 <handsomewgb@163.com>
2017-08-18 16:00:55 +0800

    深拷贝普通版

--- String_new/String_new/main.cpp
+++ String_new/String_new/main.cpp
@@ -3,6 +3,7 @@
 #include"string_ref_static.h"
 #include"string_ref_pointer.h"
 #include"string_copy_on_write.h"
+#include"string_deep_copy_one.h"
 
 int main()
 {
@@ -11,5 +12,6 @@ int main()
 	Test_static_count();
 	Test_pointer_count();
 	Test_copy();
+	Test_d1();
 	return 0;
 }
---
+++ String_new/String_new/string_deep_copy_one.h
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma once
+#include<string>
+//Éî¿½±´
+
+class String_s
+{
+public:
+	String_s(const char* str = "")
+		:_str(new char[strlen(str) + 1])
+	{
+		if (*str == 0)
+		{
+			*_str = '\0';
+		}
+		else
+		{
+			strcpy(_str, str);
+		}
+	}
+
+	String_s(const String_s& ss)
+		:_str(new char[strlen(ss._str) + 1])
+	{
+		strcpy(_str, ss._str);
+	}
+
+	String_s& operator=(const String_s& ss)
+	{
+		if (this != &ss)
+		{
+			char* tmp = new char[strlen(ss._str) + 1];
+			strcpy(tmp, ss._str);
+			delete[] _str;
+			_str = tmp;
+		}
+		return *this;
+	}
+
+	~String_s()
+	{
+		if (_str)
+		{
+			delete[] _str;
+		}
+	}
+private:
+	char* _str;
+};
+
+void Test_d1()
+{
+	String_s s1("hello wgb");
+	String_s s2(s1);
+	String_s s3 = s2;
+	String_s s4;
+	s4 = s3;
+}
